1c2a3cf5645f0da705351ff06235407b202727bd
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-03-01 00:20:00 +0900

    profile.html 추가했음

--- app.py
+++ app.py
@@ -52,8 +52,8 @@ def login():
                 'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=100)
                 }
                 token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-                #return jsonify({'result': 'success', 'token': token})
-                return render_template('success.html')
+
+                return render_template('profile.html')
                 
             else:
                 flash("비밀번호가 일치하지 않습니다.")
@@ -64,7 +64,9 @@ def login():
     else:
         return render_template('login.html')
 
-
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
