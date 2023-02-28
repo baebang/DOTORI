9f2f489cc84f2d990ce47125d85fe6e673e60fdb
Kimjaejung <67777124+baebang@users.noreply.github.com>
2023-03-01 01:23:02 +0900

    [add] 퀴즈 버튼 기능

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
 from pymongo import MongoClient
 
 from flask import Flask, render_template, jsonify, request
-
+import random
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
-db = client.dbjungle
+db = client.dotorilocal
 
 
 # HTML 화면 보여주기
 @app.route('/')
 def home():
+    quiz_set =db.quizzes.find_one({'qNnum':5})
+    question=quiz_set['question']
+    option1=quiz_set['option1']
+    option2=quiz_set['option2']
+    option3=quiz_set['option3']
+    ption4=quiz_set['option4']
+
     return render_template('multiple_choice.html')
+    # db 문제 길이를 함수에 저장
+    # quiz_num=6
+    # (난수값 생성)
+    # random_numbers = random.sample(range(1,quiz_num), 5)
+
+    # for i in random_numbers:
+        # 숫자에 맞는 퀴즈를 가져온다 'qNnum':2 << 이 번호를 random_numbers[1]
+        # quiz_set =db.quizzes.find_one({'qNnum':random_numbers[0]})
+
+        # question=quiz_set['question']
+        # option1=quiz_set['option1']
+        # option2=quiz_set['option2']
+        # option3=quiz_set['option3']
+        # option4=quiz_set['option4']
+
+        # 유저가 정답에 맞는 버튼을 누를경우 킵고잉
+        # 틀릴경우 일단 fales 화면으로 이동
+
+        
 
 @app.route('/multiple_choice_false')
 def fales():
@@ -22,50 +48,17 @@ def ddd():
     return render_template('descriptive.html')
 
 @app.route('/descriptive_flase')
-def fff():
+def descriptive_flase():
     return render_template('descriptive_flase.html')
 
 @app.route('/success')
 def sucess():
     return render_template('success.html')
 
-# API 역할을 하는 부분
-# @app.route('/api/list', methods=['GET'])
-# def show_stars():
-#     # 1. db에서 mystar 목록 전체를 검색합니다. ID는 제외하고 like 가 많은 순으로 정렬합니다.
-#     # 참고) find({},{'_id':False}), sort()를 활용하면 굿!
-#     stars = list(db.mystar.find({}, {'_id': False}).sort('like', -1))
-#     # 2. 성공하면 success 메시지와 함께 stars_list 목록을 클라이언트에 전달합니다.
-#     return jsonify({'result': 'success', 'stars_list': stars})
 
 
-# @app.route('/api/like', methods=['POST'])
-# def like_star():
-#     # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
-#     name_receive = request.form['name_give']
-
-#     # 2. mystar 목록에서 find_one으로 name이 name_receive와 일치하는 star를 찾습니다.
-#     star = db.mystar.find_one({'name': name_receive})
-#     # 3. star의 like 에 1을 더해준 new_like 변수를 만듭니다.
-#     new_like = star['like'] + 1
-
-#     # 4. mystar 목록에서 name이 name_receive인 문서의 like 를 new_like로 변경합니다.
-#     # 참고: '$set' 활용하기!
-#     db.mystar.update_one({'name': name_receive}, {'$set': {'like': new_like}})
-
-#     # 5. 성공하면 success 메시지를 반환합니다.
-#     return jsonify({'result': 'success'})
-
-
-# @app.route('/api/delete', methods=['POST'])
-# def delete_star():
-#     # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
-#     name_receive = request.form['name_give']
-#     # 2. mystar 목록에서 delete_one으로 name이 name_receive와 일치하는 star를 제거합니다.
-#     db.mystar.delete_one({'name': name_receive})
-#     # 3. 성공하면 success 메시지를 반환합니다.
-#     return jsonify({'result': 'success'})
-
+# db.quizzes.insert_one({'qNnum': 1, 'question': "question", 'option1': "option1", 'option2': "option2", 'option3': "option3", 'option4': "option4",'answer': "answer", 'explanation': "explanation"});
+# db.quizzes.insert_one({'qNnum':7,'objYN': 0, 'question': "question", 'answer': "answer", 'explanation': "explanation"});
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
