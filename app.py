f7b6659b18194d683a1637ed6a529e7c22efa195
Kimjaejung <67777124+baebang@users.noreply.github.com>
2023-03-01 03:12:47 +0900

    [Add] 체크박스 입력

--- app.py
+++ app.py
@@ -1,41 +1,51 @@
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
-
-from flask import Flask, render_template, jsonify, request
 import random
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
 db = client.dotorilocal
 
-
-# HTML 화면 보여주기
-@app.route('/')
-def home():
-    quiz_set =db.quizzes.find_one({'qNnum':5})
-    question=quiz_set['question']
-    option1=quiz_set['option1']
-    option2=quiz_set['option2']
-    option3=quiz_set['option3']
-    ption4=quiz_set['option4']
-
-    return render_template('multiple_choice.html')
-    # db 문제 길이를 함수에 저장
-    # quiz_num=6
-    # (난수값 생성)
-    # random_numbers = random.sample(range(1,quiz_num), 5)
-
-    # for i in random_numbers:
-        # 숫자에 맞는 퀴즈를 가져온다 'qNnum':2 << 이 번호를 random_numbers[1]
-        # quiz_set =db.quizzes.find_one({'qNnum':random_numbers[0]})
-
         # question=quiz_set['question']
         # option1=quiz_set['option1']
         # option2=quiz_set['option2']
         # option3=quiz_set['option3']
         # option4=quiz_set['option4']
+# HTML 화면 보여주기
+@app.route('/',methods=['GET','POST'])
+def home():
+    # # db 문제 길이를 함수에 저장
+    quiz_num=6
+    # # (난수값 생성)
+    # random_numbers = random.sample(range(1,quiz_num), 5)
+    quiz_set =db.quizzes.find_one({'qNnum':5})
+
+    if request.method == "POST":
+        some_var = request.form.get('checks')
+        # 유저에게 받은 값
+    print(quiz_set)   
+    return render_template("multiple_choice.html", quiz_set = quiz_set)
+    
+    
+    
+
+
+    # for i in random_numbers:
+    #     # 숫자에 맞는 퀴즈를 가져온다 'qNnum':2 << 이 번호를 random_numbers[1]
+    #     quiz_set =db.quizzes.find_one({'qNnum':random_numbers[i]})
+    #     if some_var == quiz_set['answer']:
+    #          quiz_set =db.quizzes.find_one({'qNnum':random_numbers[i+1]})
+    #          return render_template('multiple_choice.html',quiz_set )
+    #     else:
+    #         return redirect(url_for("descriptive_flase"))
+
+        
+
 
         # 유저가 정답에 맞는 버튼을 누를경우 킵고잉
         # 틀릴경우 일단 fales 화면으로 이동
+
+    # return render_template('multiple_choice.html')
 
         
 
