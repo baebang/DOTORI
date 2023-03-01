e12b3f41d58e207667ce0f0b39c74ea42013352c
Kimjaejung <67777124+baebang@users.noreply.github.com>
2023-03-01 16:26:57 +0900

    baebang merge into main

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify, url_for, session, re
 from pymongo import MongoClient
 from werkzeug.utils import secure_filename
 import hashlib, datetime, jwt
+import random
 
 client = MongoClient('localhost', 27017)
 db = client.dotorilocal
@@ -215,6 +216,43 @@ def ranking():
 
     return render_template("ranking.html", userslist = userslist)
 
+
+###########################################
+#
+#
+# 문제뱅킹 : 김재정
+#
+#
+###########################################
+
+@app.route('/descriptive',methods=['GET', 'POST'])
+def descriptive():
+    quiz_num=6
+    set_question=[]
+    # (난수값 생성)
+    some_var = None
+    random_numbers = random.sample(range(1,quiz_num), 5)
+
+
+    print("================random_numbers=",random_numbers)
+    print("==========answer",some_var)
+
+    for i in range(len(random_numbers)) :
+    
+        quiz_set =db.quizzes.find_one({'qNnum':random_numbers[i]})
+        set_question.insert(i, quiz_set)
+
+    
+    return render_template('descriptive.html',set_question=set_question)
+    
+
+@app.route('/descriptive_false')
+def descriptive_flase():
+    return render_template('descriptive_false.html')
+
+@app.route('/success')
+def sucess():
+    return render_template('success.html')
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
