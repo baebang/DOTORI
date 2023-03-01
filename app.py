48ab34958d70a5a9632b7337868e7ce835b5d378
Kimjaejung <67777124+baebang@users.noreply.github.com>
2023-03-01 14:59:54 +0900

    [add] 기능 테스트

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 import random
 app = Flask(__name__)
 
+
 client = MongoClient('localhost', 27017)
 db = client.dotorilocal
 
@@ -12,40 +13,29 @@ db = client.dotorilocal
         # option3=quiz_set['option3']
         # option4=quiz_set['option4']
 # HTML 화면 보여주기
-@app.route('/',methods=['GET','POST'])
+@app.route('/',methods=['GET', 'POST'])
 def home():
-    # # db 문제 길이를 함수에 저장
+    # db 문제 길이를 함수에 저장
     quiz_num=6
+    set_question=[]
     # # (난수값 생성)
-    # random_numbers = random.sample(range(1,quiz_num), 5)
-    quiz_set =db.quizzes.find_one({'qNnum':5})
+    some_var = None
+    random_numbers = random.sample(range(1,quiz_num), 5)
 
     if request.method == "POST":
         some_var = request.form.get('checks')
         # 유저에게 받은 값
-    print(quiz_set)   
-    return render_template("multiple_choice.html", quiz_set = quiz_set)
+
+    print("================random_numbers=",random_numbers)
+    print("==========answer",some_var)
+
+    for i in range(len(random_numbers)) :
     
+        quiz_set =db.quizzes.find_one({'qNnum':random_numbers[i]})
+        set_question.insert(i, quiz_set)
+
     
-    
-
-
-    # for i in random_numbers:
-    #     # 숫자에 맞는 퀴즈를 가져온다 'qNnum':2 << 이 번호를 random_numbers[1]
-    #     quiz_set =db.quizzes.find_one({'qNnum':random_numbers[i]})
-    #     if some_var == quiz_set['answer']:
-    #          quiz_set =db.quizzes.find_one({'qNnum':random_numbers[i+1]})
-    #          return render_template('multiple_choice.html',quiz_set )
-    #     else:
-    #         return redirect(url_for("descriptive_flase"))
-
-        
-
-
-        # 유저가 정답에 맞는 버튼을 누를경우 킵고잉
-        # 틀릴경우 일단 fales 화면으로 이동
-
-    # return render_template('multiple_choice.html')
+    return render_template('multiple_choice.html',set_question=set_question)
 
         
 
