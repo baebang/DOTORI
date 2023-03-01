58ee459f25f1d26e194ca1a89dfe2d2252e78ced
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-03-01 15:52:46 +0900

    문제 출제 기능 main 내 merge

--- app.py
+++ app.py
@@ -6,16 +6,27 @@ import hashlib, datetime, jwt
 client = MongoClient('localhost', 27017)
 db = client.dotorilocal
 
-
 app = Flask(__name__)
 
 SECRET_KEY = 'DOTORI'
 app.secret_key = 'DOTORI'
 
+### 추후 계정 연결 시 수정해야 되는 부분
+TEST_MADEBY_ID = 'test'
+
 # index 페이지
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+###########################################
+#
+#
+# 로그인 기능 구현: 조민기
+#
+#
+###########################################
     
 
 @app.route('/register', methods=['POST', 'GET'])
@@ -27,8 +38,8 @@ def register():
         password = request.form['password']
         password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
-        f = request.files['ex_file']
-        f.save(secure_filename(f.filename))
+        #f = request.files['ex_file']
+        #f.save(secure_filename(f.filename))
         
         member = {'username':username, 'nickname':nickname, 'userid':userid, 'password':password_hash, 'point':0}
         db.users.insert_one(member)
@@ -72,10 +83,6 @@ def login():
     else:
         return render_template('login.html')
     
-    
-    
-
-
 @app.route('/profile', methods=['GET'])
 def profile():
     
@@ -89,9 +96,6 @@ def editprofile():
     else:
         return render_template('editprofile.html')
 
-
-
-
 # 토큰 삭제가 안됨!!!
 @app.route('/logout')
 def logout():
@@ -99,6 +103,101 @@ def logout():
     # payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
     session.pop('userid', None)
     return redirect(url_for('/'))
+
+
+###########################################
+#
+#
+# 퀴즈 출제 기능 구현: 이현지
+#
+#
+###########################################
+@app.route('/newquiz', methods=['GET'])
+def newQuiz():
+    return render_template("newquiz.html");
+
+# 객관식 문항 추가
+@app.route('/quizobj', methods=['GET','POST'])
+def quizObj():
+    if request.method == "POST":
+        question = request.form["question"]
+        
+        answer = request.form["answer"]
+        option1 = request.form["option1"]
+        option2 = request.form["option2"]
+        option3 = request.form["option3"]
+        option4 = request.form["option4"]
+        answer = request.form["answer"]
+        explanation = request.form["explanation"]
+
+        #계정 연결되면 madeBy(문제 출제자 id 가져오기)도 추가가 되어야 함
+        
+        #madeBy = request.form["madeBy"]
+        #db.quizzes.insert_one({'objYN': 0, 'question': question, 'answer': answer, 'explanation': explanation, 'madeBy': madeBy});
+        db.quizzes.insert_one({'objYN': 1, 'question': question, 'option1': option1, 'option2': option2, 'option3': option3, 'option4': option4,'answer': answer, 'explanation': explanation, 'madeBy': TEST_MADEBY_ID});
+        
+        # 문제 출제자는 점수를 3점 올려준다
+        dbuser = db.users.find_one({"userid": TEST_MADEBY_ID})
+        prevPoint = dbuser["point"]
+
+        db.users.update_one({'userid': TEST_MADEBY_ID}, {'$set': {'point': prevPoint + 3}})
+
+        return redirect(url_for("index"))
+    else:
+        return render_template("quizobj.html");
+
+# 주관식 문항 추가
+@app.route('/quizsubj', methods=['GET', 'POST'])
+def quizSubj():
+    if request.method == "POST":
+        question = request.form["question"]
+        answer = request.form["answer"]
+        explanation = request.form["explanation"]
+
+        #계정 연결되면 madeBy(문제 출제자 id 가져오기)도 추가가 되어야 함
+        
+        #madeBy = request.form["madeBy"]
+        #db.quizzes.insert_one({'objYN': 0, 'question': question, 'answer': answer, 'explanation': explanation, 'madeBy': madeBy});
+        
+        db.quizzes.insert_one({'objYN': 0, 'question': question, 'answer': answer, 'explanation': explanation, 'madeBy': TEST_MADEBY_ID});
+
+        # 문제 출제자는 점수를 3점 올려준다
+        dbuser = db.users.find_one({"userid": TEST_MADEBY_ID})
+        prevPoint = dbuser["point"]
+
+        db.users.update_one({'userid': TEST_MADEBY_ID}, {'$set': {'point': prevPoint + 3}})
+
+        return redirect(url_for("index"))
+    else:
+        return render_template("quizsubj.html")
+
+@app.route('/quizlist', methods=['GET'])
+# 퀴즈 조회, 퀴즈 업데이트, 퀴즈 삭제
+def quizlist():
+    if request.method == "GET":
+        # 1. 현재 사용자의 아이디를 가져온다
+        currentid = 'test'
+        # 2. 현 사용자 아이디와 일치하는 문제들을 가져온다
+        madequiz = list(db.quizzes.find({"madeBy": currentid}))
+        # 3. 넘겨준다
+        return render_template("quizlist.html", madequiz = madequiz)
+    
+@app.route('/quizlist/delete', methods=['GET','POST'])
+def quizdelete():
+    if request.method == "POST":
+        question = request.form('question')
+        deletequiz = db.quizzes.find_one({'question': question})
+        return redirect(url_for("quizlist"))
+    else:
+        return render_template("quizlist.html")
+
+@app.route('/quizlist/modify/<id>', methods=['GET','POST'])
+def quizmodify():
+    question = request.values.get('question')
+    if request.method == "POST":
+        return redirect(url_for("quizlist"))
+    else:
+        return render_template("quizlist.html")
 
 
 if __name__ == '__main__':  
