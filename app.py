47b20bbda288259e9a0f90ac972341748b1ff9a0
Kimjaejung <67777124+baebang@users.noreply.github.com>
2023-03-01 19:41:20 +0900

    [Add] switch button

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
-from flask import Flask, render_template, request, redirect, url_for
-from pymongo import MongoClient
-import random
-app = Flask(__name__)
-=======
 from flask import Flask, render_template, request, jsonify, url_for, session, redirect, flash
 from pymongo import MongoClient
 from werkzeug.utils import secure_filename
 import hashlib, datetime, jwt
 import random
->>>>>>> main
 
 client = MongoClient('localhost', 27017)
 db = client.dotorilocal
 
-<<<<<<< HEAD
-@app.route('/')
-def index():
-    return render_template("index.html")
-=======
 app = Flask(__name__)
 
 SECRET_KEY = 'DOTORI'
@@ -236,7 +224,6 @@ def ranking():
 #
 #
 ###########################################
->>>>>>> main
 
 @app.route('/descriptive',methods=['GET', 'POST'])
 def descriptive():
@@ -245,6 +232,7 @@ def descriptive():
     # (난수값 생성)
     some_var = None
     random_numbers = random.sample(range(1,quiz_num), 5)
+    
 
 
     print("================random_numbers=",random_numbers)
@@ -255,22 +243,17 @@ def descriptive():
         quiz_set =db.quizzes.find_one({'qNnum':random_numbers[i]})
         set_question.insert(i, quiz_set)
 
-    
+    descriptive_flase(set_question)
     return render_template('descriptive.html',set_question=set_question)
     
 
 @app.route('/descriptive_false')
-def descriptive_flase():
+def descriptive_flase(nset_questionum):
     return render_template('descriptive_false.html')
 
 @app.route('/success')
 def sucess():
     return render_template('success.html')
 
-<<<<<<< HEAD
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-=======
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
->>>>>>> main
